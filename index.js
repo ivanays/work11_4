b6d9a5eebeee045bf1641a42ca52edc3e5f33d12
ivanays <ivanays@yandex.ru>
2023-01-19 22:37:13 +0300

    work11_4 v1.05 models testing

--- index.js
+++ index.js
@@ -129,6 +129,15 @@ const Comment = sequelize.define(
     }
 );
 
+; (async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+})();
+
 // ; (async () => {
 //     try {
 //         await User.sync({
